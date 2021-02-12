d356df2467bee30bbc5f62e8f53fbbb8d07b2eca
i-ognev <crosswork20>
2021-02-12 21:04:40 +0500

    1

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -2,6 +2,7 @@ from aiogram import types
 from loader import dp
 
 # sdfg sdfs dfgs dfg
+# sdg sdfgsdfgdsfg
 @dp.message_handler()
 async def bot_echo(message: types.Message):
     await message.answer(message.text)
