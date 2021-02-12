496a129d05a29bb0a19f15280e888639851a0dc7
i-ognev <Crosswork20>
2021-02-12 21:52:04 +0500

    start

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -2,9 +2,14 @@ from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
 from loader import dp
-# dsf g dsfg sdf
-# fdg dfhh
+
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f'Привет, {message.from_user.full_name}!')
+    await message.answer_photo("https://ladyle.ru/images/ladyle/2020/04/ddvn-sfxkaa_p4t.jpg",
+                               "👋👋👋 Приветствуем вас в нашем телеграмм-боте 2touch. "
+                               "Здесь мы поможем вам составить заявку для обращения к нашим услугам.\n\n"
+                               "Для начала давайте познакомимся!\n\n"
+                               "Как к вам обращаться? ⤵")
+
+
